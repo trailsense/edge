5c17db67bbde2e66309543138343f5b898b6bb2c
francesco <francesco@lanthaler.com>
2026-01-16 18:47:05 +0100

    feat: initialize esp32 rust project and setup simple sniffing mode

---
+++ trailsense-edge/src/bin/main.rs
@@ -0,0 +1,76 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use esp_backtrace as _;
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::peripherals::Peripherals;
+
+use esp_hal::time::{Duration, Instant};
+use esp_hal::timer::timg::TimerGroup;
+use esp_radio::ble::controller::BleConnector;
+use esp_radio::wifi::PromiscuousPkt;
+use log::info;
+
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    // generator version: 1.1.0
+
+    let peripherals = init_hardware();
+
+    esp_println::logger::init_logger_from_env();
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
+    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
+
+    let _transport = BleConnector::new(&radio_init, peripherals.BT, Default::default()).unwrap();
+    let (mut _wifi_controller, interfaces) =
+        esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
+            .expect("Failed to initialize Wi-Fi controller");
+
+    let mut device = interfaces.sniffer;
+
+    device
+        .set_promiscuous_mode(true)
+        .expect("Failed to set wifi into sniffer mode");
+
+    device.set_receive_cb(read_packet);
+
+    loop {
+        info!("Hello Trailsense team!");
+        blocking_delay(Duration::from_millis(500));
+    }
+}
+
+fn blocking_delay(duration: Duration) {
+    let delay_start = Instant::now();
+    while delay_start.elapsed() < duration {}
+}
+
+fn init_hardware() -> Peripherals {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+    esp_alloc::heap_allocator!(size: 72 * 1024);
+    peripherals
+}
+
+fn read_packet(packet: PromiscuousPkt) {
+    info!("Received packet: {} bytes", packet.len);
+}
