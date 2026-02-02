306dda9e91f4cdcfe71928958bbedbc2e30d5a5f
francesco <francesco@lanthaler.com>
2026-02-02 21:00:56 +0100

    fix: update documentation for fingerprint_probe function and correct initial fingerprint value

--- trailsense-edge/src/probe_parser.rs
+++ trailsense-edge/src/probe_parser.rs
@@ -1,18 +1,21 @@
 extern crate alloc;
 use crate::models::MODEL;
 
-/** # Fingerprint Probe
-* Generate a fingerprint for the given probe data using the defined filters generated with a python script.
-* Each filter outputs a single bit, which are concatenated to form the final fingerprint.
-
-## Arguments
-* `data` - A byte slice representing the probe data to be fingerprinted.
-## Returns
-* A u16 variable is returned, where each bit represents one bit of the filter.
-*/
+/// # Fingerprint Probe
+///
+/// Generate a fingerprint for the given probe data using the defined filters generated with a python script.
+/// Each filter outputs a single bit, which are concatenated to form the final fingerprint.
+///
+/// # Arguments
+///
+/// * `data` - A byte slice representing the probe data to be fingerprinted.
+///
+/// # Returns
+///
+/// A `u16` value is returned, where each bit represents one bit of the filter.
 pub fn fingerprint_probe(data: &[u8]) -> u16 {
     // Change to u32 or as needed if increasing filter size.
-    let mut fingerprint = 0b000u16;
+    let mut fingerprint = 0u16;
 
     for (idx, model) in MODEL.iter().enumerate() {
         let max_iterations = core::cmp::min(data.len(), model.mask.len());
@@ -32,3 +35,4 @@ pub fn fingerprint_probe(data: &[u8]) -> u16 {
 
     fingerprint
 }
+∂
